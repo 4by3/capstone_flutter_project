fd833715ccbb8c7c3a4e1b907a9448bd41407c21
4by3 <timothywastaken@hotmail.com>
2025-03-19 17:45:08 +1100

    Popup for home page

---
+++ lib/widgets/video_popup.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPopup extends StatefulWidget {
+  final String videoUrl;
+
+  const VideoPopup({super.key, required this.videoUrl});
+
+  @override
+  _VideoPopupState createState() => _VideoPopupState();
+}
+
+class _VideoPopupState extends State<VideoPopup> {
+  late VideoPlayerController _controller;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+        _isPlaying = true;
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _controller.value.isInitialized
+                ? Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _isPlaying = !_isPlaying;
+                            _isPlaying ? _controller.play() : _controller.pause();
+                          });
+                        },
+                        child: AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Slider(
+                        min: 0,
+                        max: _controller.value.duration.inSeconds.toDouble(),
+                        value: _controller.value.position.inSeconds.toDouble(),
+                        onChanged: (value) {
+                          setState(() {
+                            _controller.seekTo(Duration(seconds: value.toInt()));
+                          });
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow, size: 30),
+                            onPressed: () {
+                              setState(() {
+                                _isPlaying = !_isPlaying;
+                                _isPlaying ? _controller.play() : _controller.pause();
+                              });
+                            },
+                          ),
+                          Text(
+                            "${_controller.value.position.inMinutes}:${(_controller.value.position.inSeconds % 60).toString().padLeft(2, '0')} / "
+                            "${_controller.value.duration.inMinutes}:${(_controller.value.duration.inSeconds % 60).toString().padLeft(2, '0')}",
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                : const CircularProgressIndicator(),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
