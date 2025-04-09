e008a2e7e0f3d7e0a45108454e31636c17fcb3a3
4by3 <timothywastaken@hotmail.com>
2025-04-10 01:32:29 +1000

    Changed intro page look

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -18,7 +18,7 @@ class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    
+
     _controller = AnimationController(
       duration: const Duration(milliseconds: 400),
       vsync: this,
@@ -69,128 +69,160 @@ class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
                     scale: _scaleAnimation,
                     child: Column(
                       children: [
-                        Image.asset(
-                          'assets/images/logo.png',
-                          width: 350,
-                          height: 350,
-                        ),
-                        const SizedBox(height: 24),
+                        const SizedBox(height: 54),
                         const Text(
                           "Welcome,",
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontSize: 18,
-                            color: Color.fromARGB(255, 24, 53, 98),
+                            color: Color.fromARGB(255, 10, 35, 73),
                           ),
                         ),
-                        const SizedBox(height: 24),
-                        const Text(
-                          "When it's about Privacy,\nAwareness Matters!",
+                        const SizedBox(height: 14),
+                        RichText(
                           textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 38,
-                            color: Color.fromARGB(255, 24, 53, 98),
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        const Text(
-                          "Learn privacy settings through interactive quizzes and videos",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color.fromARGB(255, 24, 53, 98),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 32),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blue.withOpacity(0.1),
-                        blurRadius: 10,
-                        offset: const Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _isExpanded = !_isExpanded;
-                            _isExpanded ? _controller.forward() : _controller.reverse();
-                          });
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              colors: [Color.fromARGB(255, 109, 141, 188), Color.fromARGB(255, 24, 53, 98)],
-                            ),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          text: const TextSpan(
                             children: [
-                              const Text(
-                                "Our Mission",
+                              TextSpan(
+                                text: "When it's about ",
                                 style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
+                                  fontSize: 44,
                                   fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 24, 53, 98),
                                 ),
                               ),
-                              AnimatedRotation(
-                                turns: _isExpanded ? 0.5 : 0,
-                                duration: const Duration(milliseconds: 300),
-                                child: const Icon(
-                                  Icons.expand_more,
-                                  color: Colors.white,
+                              TextSpan(
+                                text: "Privacy, Awareness",
+                                style: TextStyle(
+                                  fontSize: 44,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.3,
+                                  color: Color(0xFF1877F2),
+                                ),
+                              ),
+                              TextSpan(
+                                text: " Matters!",
+                                style: TextStyle(
+                                  fontSize: 44,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 24, 53, 98),
                                 ),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                      AnimatedCrossFade(
-                        firstChild: Container(
-                          width: double.infinity,
-                          height: 0, // Explicitly set height to 0 when collapsed
+                        const SizedBox(height: 54),
+                        Image.asset(
+                          'assets/images/logo.png',
+                          width: 600,
+                          height: 500,
                         ),
-                        secondChild: Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.all(16),
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(16),
-                              bottomRight: Radius.circular(16),
-                            ),
-                          ),
-                          child: const Text(
-                            "Master setting up privacy, learn the Facebook features, and become a valuable member of the digital society.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Color.fromARGB(255, 24, 53, 98),
-                            ),
-                          ),
-                        ),
-                        crossFadeState: _isExpanded
-                            ? CrossFadeState.showSecond
-                            : CrossFadeState.showFirst,
-                        duration: const Duration(milliseconds: 300),
-                        sizeCurve: Curves.easeInOut,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
+
+                // Container(
+                //   decoration: BoxDecoration(
+                //     borderRadius: BorderRadius.circular(16),
+                //     boxShadow: [
+                //       BoxShadow(
+                //         color: Colors.blue.withOpacity(0.1),
+                //         blurRadius: 10,
+                //         offset: const Offset(0, 4),
+                //       ),
+                //     ],
+                // ),
+                // child: Column(
+                //   children: [
+                //     GestureDetector(
+                //       onTap: () {
+                //         setState(() {
+                //           _isExpanded = !_isExpanded;
+                //           _isExpanded ? _controller.forward() : _controller.reverse();
+                //         });
+                //       },
+                //       child: Container(
+                //         padding: const EdgeInsets.all(16),
+                //         decoration: BoxDecoration(
+                //           gradient: const LinearGradient(
+                //             colors: [Color.fromARGB(255, 26, 59, 105), Color.fromARGB(255, 84, 101, 124)],
+                //           ),
+                //           borderRadius: BorderRadius.circular(16),
+                //         ),
+                //         child: Row(
+                //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //           children: [
+                //             const Text(
+                //               "Our Mission",
+                //               style: TextStyle(
+                //                 fontSize: 18,
+                //                 color: Colors.white,
+                //                 fontWeight: FontWeight.bold,
+                //               ),
+                //             ),
+                //             AnimatedRotation(
+                //               turns: _isExpanded ? 0.5 : 0,
+                //               duration: const Duration(milliseconds: 300),
+                //               child: const Icon(
+                //                 Icons.expand_more,
+                //                 color: Colors.white,
+                //               ),
+                //             ),
+                //           ],
+                //         ),
+                //       ),
+                //     ),
+                //     AnimatedCrossFade(
+                //       firstChild: Container(
+                //         width: double.infinity,
+                //         height: 0, // Explicitly set height to 0 when collapsed
+                //       ),
+                //       secondChild: Container(
+                //         width: double.infinity,
+                //         padding: const EdgeInsets.all(16),
+                //         decoration: const BoxDecoration(
+                //           color: Colors.white,
+                //           borderRadius: BorderRadius.only(
+                //             bottomLeft: Radius.circular(16),
+                //             bottomRight: Radius.circular(16),
+                //           ),
+                //         ),
+                //         child: const Text(
+                //           "Master setting up privacy, learn the Facebook features, and become a valuable member of the digital society.",
+                //           textAlign: TextAlign.center,
+                //           style: TextStyle(
+                //             fontSize: 16,
+                //             color: Color.fromARGB(255, 24, 53, 98),
+                //           ),
+                //         ),
+                //       ),
+                //       crossFadeState: _isExpanded
+                //           ? CrossFadeState.showSecond
+                //           : CrossFadeState.showFirst,
+                //       duration: const Duration(milliseconds: 300),
+                //       sizeCurve: Curves.easeInOut,
+                //     ),
+                //   ],
+                // ),
+                // ),
+
+
+                const SizedBox(height: 32),
+
+                const Text(
+                  "Learn privacy settings through interactive\nquizzes and videos!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 10, 35, 73),
+                  ),
+                ),
+
+                const SizedBox(height: 32),
+
                 const Spacer(),
+
                 FadeTransition(
                   opacity: _fadeAnimation,
                   child: ElevatedButton(
@@ -204,12 +236,12 @@ class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
                     },
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(
-                        horizontal: 78,
-                        vertical: 30,
+                        horizontal: 118,
+                        vertical: 32,
                       ),
                       backgroundColor: const Color.fromARGB(255, 24, 53, 98),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
+                        borderRadius: BorderRadius.circular(100),
                       ),
                       elevation: 8,
                     ),
@@ -219,7 +251,7 @@ class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
                         Text(
                           "Start Learning",
                           style: TextStyle(
-                            fontSize: 20,
+                            fontSize: 30,
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                           ),
@@ -230,7 +262,7 @@ class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 40),
               ],
             ),
           ),
